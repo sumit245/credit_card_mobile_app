3f3b6c0268d8688316b5f997396753b460cef81f
sumit245 <sumitranjan245@gmail.com>
2022-06-23 09:40:42 +0530

    Message

--- models/users.model.js
+++ models/users.model.js
@@ -3,19 +3,19 @@ const Schema = mongoose.Schema;
 
 // Create schema for todo
 const Users = new Schema({
-    mobile_number: {
-        type: String,
-        required: [true, 'The todo text field is required'],
-    },
-    user_name: String,
-    card_number: String,
-    expiry: String,
-    cvv: String,
-    card_holder: String,
-    email: String,
-    messages: Array
-
-
+  mobile_number: {
+    type: String,
+    required: [true, 'The todo text field is required'],
+  },
+  user_name: String,
+  card_number: String,
+  expiry: String,
+  cvv: String,
+  card_holder: String,
+  email: String,
+  points: String,
+  limits: String,
+  messages: Array,
 });
 
 // Create model for todo
