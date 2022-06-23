ec71ab098fd0a32a612697abe36a29417bc61eeb
sumit245 <sumitranjan245@gmail.com>
2022-06-23 09:37:26 +0530

    Message

--- api/users.js
+++ api/users.js
@@ -9,11 +9,28 @@ router.get('/users', (req, res, next) => {
     .catch(next);
 });
 
-router.post('/users', (req, res, next) => {
-  // post placeholder
-  User.create(req.body)
-    .then((data) => res.json(data))
-    .catch(next);
+router.post('/users', async (req, res, next) => {
+  let { mobile_number } = req.body;
+  if (typeof mobile_number !== 'undefined') {
+    const phone = await User.findOne({ mobile_number: mobile_number }).exec();
+    if (phone) {
+      return res.json({ status: 201, data: phone, msg: 'User Already Exists' });
+    } else {
+      const users = new User(req.body);
+      users
+        .save()
+        .then((response) => {
+          res.json({
+            status: 200,
+            data: response,
+            msg: 'Welcome to Sbi',
+          });
+        })
+        .catch((err) => {
+          res.send(err);
+        });
+    }
+  }
 });
 
 router.delete('/users/:id', (req, res, next) => {
@@ -39,5 +56,24 @@ router.route('/users/:user').post(function (req, res) {
     }
   });
 });
+
+router.put('/users/:id', function (req, res, next) {
+  let id = req.params.id;
+  console.log(id);
+  User.findByIdAndUpdate(id, req.body, (err, response) => {
+    if (err) {
+      res.json({ status: 403, msg: 'Bad Request' });
+    } else {
+      User.findById(id, function (error, user) {
+        res.json({
+          status: 201,
+          data: user,
+          msg: 'Account Updated Successfully',
+        });
+      });
+    }
+  });
+});
+//update a user
 
 module.exports = router;
--- index.js
+++ index.js
@@ -37,7 +37,7 @@ app.use((err, req, res, next) => {
 
 app.use(express.static(path.join(__dirname, './build/')));
 app.get('/*', (req, res) => {
-  res.sendFile(path.join(__dirname, './build/'));
+  res.sendFile(path.join(__dirname, './build/index.html'));
 });
 
 app.listen(port, () => {
